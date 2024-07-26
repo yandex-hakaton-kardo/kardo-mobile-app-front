ff750483c079603d3e06c5dd6c36efccfa81cb19
Andrey Voronin <AVor0n@yandex.ru>
2024-07-26 10:37:46 +0300

    feat: add langs provider

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,6 +1,13 @@
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import { LangProvider } from 'context';
 import { routes } from './routes';
 
-const App = () => <RouterProvider router={createBrowserRouter(routes)} />;
+const router = createBrowserRouter(routes);
+
+const App = () => (
+  <LangProvider>
+    <RouterProvider router={router} />
+  </LangProvider>
+);
 
 export default App;
