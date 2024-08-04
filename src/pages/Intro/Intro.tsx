0c9c4c1c8237439a3d7ee8374c9c37bd95d96455
dmitry.mazhnik <dmitry-mazhnik@yandex.ru>
2024-08-04 14:57:37 +0300

    add intro

--- src/pages/Intro/Intro.tsx
+++ src/pages/Intro/Intro.tsx
@@ -1,1 +1,122 @@
-export const Intro = () => <div>IntroPage</div>;
+import { Button } from '@components';
+import styles from './Intro.module.scss';
+// import { useLang } from "context";
+import { Preloader } from 'components/Preloader';
+import { useEffect, useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+
+export const Intro = () => {
+  interface TState {
+    title: string | null;
+    subtitle: string | null;
+    text: string;
+    button: string | null;
+    isLoading: boolean;
+    page: number;
+  }
+
+  const navigate = useNavigate();
+
+  const [state, setState] = useState<TState>({
+    title: 'Международная премия',
+    subtitle: 'Кардо',
+    text: 'Улица начинается здесь',
+    button: null,
+    isLoading: true,
+    page: 0,
+  });
+
+  function nextPage() {
+    setTimeout(() => {
+      setState({
+        title: '',
+        subtitle: '',
+        text: 'Регистрируйся на соревнования быстро и просто',
+        button: 'Далее',
+        isLoading: false,
+        page: 1,
+      });
+    }, 2500);
+  }
+
+  useEffect(() => {
+    nextPage();
+  }, []);
+
+  const clickButton = () => {
+    const newPage = state.page + 1;
+    if (state.page === 1) {
+      setState(prevState => ({
+        ...prevState,
+        page: newPage,
+        text: 'Листай ленту, смотри видео и болей за любимого спортсмена',
+      }));
+    } else if (state.page === 2) {
+      setState(prevState => ({
+        ...prevState,
+        page: newPage,
+        text: 'Настрой уведомления, чтобы не пропускать онлайн-трансляции соревнований',
+      }));
+    } else {
+      navigate('/');
+    }
+  };
+
+  if (!state.isLoading) {
+    return (
+      <>
+        <div className={styles.video_wrap}>
+          <video poster="/images/poster.jpeg" className={styles.video_bg} autoPlay muted loop>
+            <source src="/videos/skateboarding.webm" type="video/webm" />
+            <source src="/videos/skateboarding.mp4" type="video/mp4" />
+          </video>
+          <div className={styles.container_video}>
+            <p className={styles.text_for_video}>{state.text}</p>
+            <div className={styles.strip}>
+              {state.page === 1 && (
+                <>
+                  <div className={styles.strip_rectangle1}></div>
+                  <div className={styles.strip_rectangle}></div>
+                  <div className={styles.strip_rectangle}></div>
+                </>
+              )}
+              {state.page === 2 && (
+                <>
+                  <div className={styles.strip_rectangle}></div>
+                  <div className={styles.strip_rectangle1}></div>
+                  <div className={styles.strip_rectangle}></div>
+                </>
+              )}
+              {state.page === 3 && (
+                <>
+                  <div className={styles.strip_rectangle}></div>
+                  <div className={styles.strip_rectangle}></div>
+                  <div className={styles.strip_rectangle1}></div>
+                </>
+              )}
+            </div>
+            <Button view="action" size="large" className={styles.button_for_video} onClick={clickButton}>
+              {state.button}
+            </Button>
+            <Link to={'/'}>
+              <p className={styles.text2}>Пропустить</p>
+            </Link>
+          </div>
+        </div>
+      </>
+    );
+  }
+
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.header}>{state.title}</h1>
+      <h2 className={styles.header2}>{state.subtitle}</h2>
+      <p className={styles.text}>{state.text}</p>
+      {state.isLoading && (
+        <div style={{ position: 'absolute', bottom: '21%' }}>
+          <Preloader />
+        </div>
+      )}
+    </div>
+  );
+};
