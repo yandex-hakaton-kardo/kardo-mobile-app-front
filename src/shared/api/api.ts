8541ce48327f5f9992c5242c5ddaa7a035672078
Andrey Voronin <AVor0n@yandex.ru>
2024-08-06 18:57:30 +0300

    fix: refresh token

--- src/shared/api/api.ts
+++ src/shared/api/api.ts
@@ -1,20 +1,77 @@
-import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
-import { type StoreSchema } from 'app/store';
+import {
+  type BaseQueryFn,
+  type FetchArgs,
+  type FetchBaseQueryError,
+  createApi,
+  fetchBaseQuery,
+} from '@reduxjs/toolkit/query/react';
+import { LsKeys } from '@shared/constants';
+import { authActions } from 'entities/Auth.slice';
 
-const baseQuery = fetchBaseQuery({
+let isRefreshing = false;
+let refreshPromise: Promise<unknown> | null = null;
+
+const baseQueryWithAuth = fetchBaseQuery({
   baseUrl: `/api`,
-  prepareHeaders: (headers, { getState }) => {
-    const { accessToken } = (getState() as StoreSchema).auth;
+  prepareHeaders: headers => {
+    const accessToken = localStorage.getItem(LsKeys.ACCESS_TOKEN);
 
     if (accessToken) {
       headers.set('Authorization', `Bearer ${accessToken}`);
     }
+
+    headers.set('Accept', 'application/json');
+    headers.set('Cache-Control', 'no-cache');
+    headers.set('Pragma', 'no-cache');
+    headers.set('Expires', '0');
+
     return headers;
   },
-  credentials: 'include',
 });
 
+const baseQueryWithReauth: BaseQueryFn<string | FetchArgs, unknown, FetchBaseQueryError> = async (
+  args,
+  api,
+  extraOptions,
+) => {
+  const tokenExpirationDate = localStorage.getItem(LsKeys.ACCESS_TOKEN_EXPIRED);
+  if (!tokenExpirationDate || new Date(tokenExpirationDate) > new Date()) {
+    return baseQueryWithAuth(args, api, extraOptions);
+  }
+  if (!isRefreshing) {
+    isRefreshing = true;
+    refreshPromise = fetch('/api/users/tokens/refresh', {
+      method: 'POST',
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem(LsKeys.REFRESH_TOKEN)}`,
+      },
+    })
+      .then(res => res.json())
+      .then(refreshResult => {
+        if (refreshResult) {
+          const refreshTokenResult = refreshResult as { accessToken: string; accessTokenExpiry: string };
+
+          localStorage.setItem(LsKeys.ACCESS_TOKEN, refreshTokenResult.accessToken);
+          localStorage.setItem(LsKeys.ACCESS_TOKEN_EXPIRED, refreshTokenResult.accessTokenExpiry);
+          isRefreshing = false;
+
+          return refreshTokenResult;
+        }
+        isRefreshing = false;
+
+        return null;
+      })
+      .catch(() => {
+        isRefreshing = false;
+        api.dispatch(authActions.clear());
+      });
+  }
+
+  await refreshPromise;
+  return baseQueryWithAuth(args, api, extraOptions);
+};
+
 export const api = createApi({
-  baseQuery,
+  baseQuery: baseQueryWithReauth,
   endpoints: () => ({}),
 });
