a049fa111f7ef0e22df8ba6f94c41b9c411afe0e
Andrey Voronin <AVor0n@yandex.ru>
2024-08-07 20:55:21 +0300

    feat: add SegmentPicker

---
+++ src/components/SegmentPicker/SegmentPicker.tsx
@@ -0,0 +1,27 @@
+import clsx from 'clsx';
+import { type ReactNode } from 'react';
+import styles from './SegmentPicker.module.scss';
+
+interface SegmentPickerProps {
+  segments: {
+    id: string;
+    text: ReactNode;
+    onClick: () => void;
+  }[];
+  activeSegment: string;
+}
+
+export const SegmentPicker = ({ segments, activeSegment }: SegmentPickerProps) => (
+  <div className={styles.container}>
+    {segments.map(({ id, text, onClick }) => (
+      <button
+        key={id}
+        type="button"
+        onClick={onClick}
+        className={clsx(styles.segment, activeSegment === id && styles.active)}
+      >
+        {text}
+      </button>
+    ))}
+  </div>
+);
