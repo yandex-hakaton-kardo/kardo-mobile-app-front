15309c70de59bfaf1ef157bbf253a054373f3147
Andrey Voronin <AVor0n@yandex.ru>
2024-08-08 16:45:41 +0300

    perf: impove speed of load video

--- src/widgets/Video/Video.tsx
+++ src/widgets/Video/Video.tsx
@@ -1,32 +1,14 @@
-import clsx from 'clsx';
-import { useEffect, useRef, useState } from 'react';
+import { useState } from 'react';
 import { Skeleton } from '@components';
-import { requestWithAuth, requestWithReAuth } from '@shared/api';
-import { useAppDispatch } from 'app/store';
 import styles from './Video.module.scss';
 
 export const Video = ({ src, ...props }: JSX.IntrinsicElements['video']) => {
-  const dispatch = useAppDispatch();
-  const ref = useRef<HTMLVideoElement>(null);
-  const [status, setStatus] = useState<'none' | 'loading' | 'loaded'>('none');
-
-  useEffect(() => {
-    if (!src || status !== 'none') return;
-    setStatus('loading');
-    requestWithReAuth(dispatch, () => requestWithAuth(`/api/content/${src}`))
-      .then(res => res.blob())
-      .then(blob => {
-        if (ref.current) {
-          ref.current.src = URL.createObjectURL(blob);
-          ref.current.onloadedmetadata = () => setStatus('loaded');
-        }
-      });
-  }, [dispatch, src, status]);
+  const [loaded, setLoaded] = useState(false);
 
   return (
     <div className={styles.container}>
-      <Skeleton className={clsx(styles.skeleton, status === 'loaded' && styles.hidden)} />
-      <video ref={ref} src="" width="100%" {...props} />
+      {!loaded && <Skeleton className={styles.skeleton} />}
+      <video src={`/api/content/${src}`} width="100%" onLoadedData={() => setLoaded(true)} {...props} />
     </div>
   );
 };
