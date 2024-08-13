c8202c12f601eb673e13d4d721f1ca5e8237771f
Andrey Voronin <AVor0n@yandex.ru>
2024-08-13 09:11:43 +0300

    chore: remove excess code

--- src/shared/ui/PageLoader/PageLoader.tsx
+++
@@ -1,9 +0,0 @@
-import { Suspense } from 'react';
-import { Outlet } from 'react-router-dom';
-
-export const PageLoader = () => (
-  // TODO: реализовать экран загрузки страницы
-  <Suspense fallback="loading...">
-    <Outlet />
-  </Suspense>
-);
